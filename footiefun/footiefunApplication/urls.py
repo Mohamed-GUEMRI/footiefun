c36fbd75d9412f9a8db56cb9d79a42af5f9d4018
mohamed guemri <mohguemri17@gmail.com>
2024-04-28 21:43:57 +0100

    done with urls v1.0

--- footiefun/footiefunApplication/urls.py
+++ footiefun/footiefunApplication/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path,include
 from rest_framework import routers
+from views import ReservationViewSet,MatchViewSet,TouroitViewSet,StadeViewSet,UserViewSet
 
 router = routers.DefaultRouter()
-urlpatterns = [
-    path ('', include(router.urls)),
-    ]
+router.register(r'reservations', ReservationViewSet)
+router.register(r'matches', MatchViewSet)
+router.register(r'tournoits', TouroitViewSet)  # Note the spelling correction here
+router.register(r'stades', StadeViewSet)
+router.register(r'users', UserViewSet)
 
+urlpatterns = [
+    path('', include(router.urls)),
+]
